8c5e3352c9c0d400069a66c1edba357a7b00a54b
Chetan-Goyal <harshrock616@gmail.com>
2022-01-22 18:41:16 +0530

    Initial Commit

---
+++ lib/src/resources/movie_api_provider.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+import 'package:bloc_test_project/src/models/trailer_model.dart';
+import 'package:http/http.dart' show Client;
+import 'dart:convert';
+import '../models/item_model.dart';
+
+class MovieApiProvider {
+  Client client = Client();
+  final _apiKey = '5d7761779dadfd20e4f4a23588102058';
+  final _baseUrl = "http://api.themoviedb.org/3/movie";
+
+  Future<ItemModel> fetchMovieList() async {
+    print("entered");
+    final response = await client.get(
+      Uri.parse("$_baseUrl/popular?api_key=$_apiKey"),
+    );
+    print(response.body.toString());
+    if (response.statusCode == 200) {
+      // If the call to the server was successful, parse the JSON
+      return ItemModel.fromJson(json.decode(response.body));
+    } else {
+      // If that call was not successful, throw an error.
+      throw Exception('Failed to load post');
+    }
+  }
+
+  Future<TrailerModel> fetchTrailer(int movieId) async {
+    final response = await client
+        .get(Uri.parse("$_baseUrl/$movieId/videos?api_key=$_apiKey"));
+
+    if (response.statusCode == 200) {
+      return TrailerModel.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to load trailers');
+    }
+  }
+}
---
+++ lib/src/ui/movie_list.dart
@@ -0,0 +1,79 @@
+import 'package:bloc_test_project/src/blocs/movie_detail_bloc_provider.dart';
+import 'package:flutter/material.dart';
+import '../models/item_model.dart';
+import '../blocs/movies_bloc.dart';
+import 'movie_detail.dart';
+
+class MovieList extends StatefulWidget {
+  @override
+  State<MovieList> createState() => _MovieListState();
+}
+
+class _MovieListState extends State<MovieList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Popular Movies'),
+      ),
+      body: StreamBuilder(
+        stream: bloc.allMovies,
+        builder: (context, AsyncSnapshot<ItemModel> snapshot) {
+          if (snapshot.hasData) {
+            return buildList(snapshot);
+          } else if (snapshot.hasError) {
+            return Text(snapshot.error.toString());
+          }
+          return Center(child: CircularProgressIndicator());
+        },
+      ),
+    );
+  }
+
+  Widget buildList(AsyncSnapshot<ItemModel> snapshot) {
+    return GridView.builder(
+        itemCount: snapshot.data!.results.length,
+        gridDelegate:
+            const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        itemBuilder: (BuildContext context, int index) {
+          return InkResponse(
+            enableFeedback: true,
+            onTap: () => openDetailPage(snapshot.data!, index),
+            child: Image.network(
+              'https://image.tmdb.org/t/p/w185${snapshot.data!.results[index].poster_path}',
+              fit: BoxFit.cover,
+            ),
+          );
+        });
+  }
+
+  openDetailPage(ItemModel data, int index) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) {
+        return MovieDetailBlocProvider(
+          child: MovieDetail(
+            title: data.results[index].title,
+            posterUrl: data.results[index].backdrop_path,
+            description: data.results[index].overview,
+            releaseDate: data.results[index].release_date,
+            voteAverage: data.results[index].vote_average.toString(),
+            movieId: data.results[index].id,
+          ),
+        );
+      }),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    bloc.fetchAllMovies();
+  }
+
+  @override
+  void dispose() {
+    bloc.dispose();
+    super.dispose();
+  }
+}
